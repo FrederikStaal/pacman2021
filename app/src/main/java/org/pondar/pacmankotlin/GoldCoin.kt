9872ec468371212218a2ab05887a231ca31ea0b6
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2021-03-08 11:42:58 +0100

    Added attributes and init function to GoldCoin class

--- app/src/main/java/org/pondar/pacmankotlin/GoldCoin.kt
+++ app/src/main/java/org/pondar/pacmankotlin/GoldCoin.kt
@@ -1,4 +1,14 @@
 package org.pondar.pacmankotlin
 
 //Here you need to fill out what should be in a GoldCoin and what should the constructor be
-class GoldCoin() {}
+class GoldCoin(x: Int, y: Int) {
+    var coinx = 0
+    var coiny = 0
+
+    var taken = false
+
+    init {
+        coinx = x
+        coiny = y
+    }
+}
